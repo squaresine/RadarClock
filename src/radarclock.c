b1ad73b0e7f420a7d0b961c9175b9207e096fb34
squaresine <scott@squaresine.com>
2013-05-14 20:09:42 -0700

    V1.0-Modifications to Simplify Watchface
    -Removed Text Around Radar Screen
    -Removed Minute Hand
    -Switched Old Minute Hand to be New Hour Hand
    -"Lightened" Radar Gradient
    -Shifted Date Text Upward 5px
    -Added More Radar Targets in Background

--- src/radarclock.c
+++ src/radarclock.c
@@ -4,7 +4,7 @@
 		
 #define MY_UUID { 0x82, 0x98, 0x17, 0xE4, 0x61, 0xCD, 0x4D, 0x10, 0x89, 0xCB, 0x99, 0x52, 0xFD, 0xA8, 0xA7, 0x6A }
 PBL_APP_INFO(MY_UUID,
-             "RadarClock", "8a22a - Code, Elzor - Design",
+             "Radar Minimal", "squaresine-ModOf 8a22a/Elzor",
              1, 1, /* App version */
              RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_WATCH_FACE);
@@ -34,23 +34,16 @@ static char month_text[] = "MAY";
 
 BmpContainer background_image_container;
 
-RotBmpPairContainer minute_hand_image_container;
 RotBmpPairContainer hour_hand_image_container;
 
 void update_watch(PblTm* t){
-
-  minute_hand_image_container.layer.white_layer.rotation = TRIG_MAX_ANGLE * (t->tm_min * 6) / 360;
-  minute_hand_image_container.layer.black_layer.rotation = TRIG_MAX_ANGLE * (t->tm_min * 6) / 360;
-  minute_hand_image_container.layer.layer.frame.origin.x = (72) - (minute_hand_image_container.layer.layer.frame.size.w/2);
-  minute_hand_image_container.layer.layer.frame.origin.y = (75) - (minute_hand_image_container.layer.layer.frame.size.h/2);
-  layer_mark_dirty(&minute_hand_image_container.layer.layer);	
 
   hour_hand_image_container.layer.white_layer.rotation = TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 30) + (t->tm_min/2)) / 360;
   hour_hand_image_container.layer.black_layer.rotation = TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 30) + (t->tm_min/2)) / 360;
   hour_hand_image_container.layer.layer.frame.origin.x = (72) - (hour_hand_image_container.layer.layer.frame.size.w/2);
   hour_hand_image_container.layer.layer.frame.origin.y = (75) - (hour_hand_image_container.layer.layer.frame.size.h/2);
   layer_mark_dirty(&hour_hand_image_container.layer.layer);
-
+		
 }
 
 void draw_day(){
@@ -85,7 +78,7 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
 void handle_init(AppContextRef app_ctx) {
 
   // Create our app's base window
-  window_init(&window, "Radar Clock");
+  window_init(&window, "Radar Minimal");
   window_stack_push(&window, true);
   window_set_background_color(&window, GColorBlack);
 
@@ -95,18 +88,13 @@ void handle_init(AppContextRef app_ctx) {
   bmp_init_container(RESOURCE_ID_IMAGE_BACKGROUND, &background_image_container);
   layer_add_child(&window.layer, &background_image_container.layer.layer);
 
-  // Set up a layer for the minute hand with transparency, ensure the source image has a transparent area
-  rotbmp_pair_init_container(RESOURCE_ID_IMAGE_MINUTE_HAND_WHITE, RESOURCE_ID_IMAGE_MINUTE_HAND_BLACK, &minute_hand_image_container);
-  rotbmp_pair_layer_set_src_ic(&minute_hand_image_container.layer, GPoint(50, 55));
-  layer_add_child(&window.layer, &minute_hand_image_container.layer.layer);	
-
   // Set up a layer for the hour hand with transparency, ensure the source image has a transparent area
   rotbmp_pair_init_container(RESOURCE_ID_IMAGE_HOUR_HAND_WHITE, RESOURCE_ID_IMAGE_HOUR_HAND_BLACK, &hour_hand_image_container);
-  rotbmp_pair_layer_set_src_ic(&hour_hand_image_container.layer, GPoint(35, 38));
+  rotbmp_pair_layer_set_src_ic(&hour_hand_image_container.layer, GPoint(50, 55));
   layer_add_child(&window.layer, &hour_hand_image_container.layer.layer);
 
   day_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PIXELMIX_10));
-  text_layer_init(&day_layer, GRect(34, 150, 28, 12)); 
+  text_layer_init(&day_layer, GRect(34, 145, 28, 12)); 
   text_layer_set_text_alignment(&day_layer, GTextAlignmentCenter);
   text_layer_set_text_color(&day_layer, GColorWhite);
   text_layer_set_background_color(&day_layer, GColorClear);
@@ -115,7 +103,7 @@ void handle_init(AppContextRef app_ctx) {
   draw_day();
 
   date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PIXELMIX_10));
-  text_layer_init(&date_layer, GRect(63, 150, 21, 12));
+  text_layer_init(&date_layer, GRect(63, 145, 21, 12));
   text_layer_set_text_alignment(&date_layer, GTextAlignmentCenter);
   text_layer_set_text_color(&date_layer, GColorWhite);
   text_layer_set_background_color(&date_layer, GColorClear);
@@ -124,7 +112,7 @@ void handle_init(AppContextRef app_ctx) {
   draw_date();	
 
   month_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PIXELMIX_10));
-  text_layer_init(&month_layer, GRect(84, 150, 28, 12));
+  text_layer_init(&month_layer, GRect(84, 145, 28, 12));
   text_layer_set_text_alignment(&month_layer, GTextAlignmentCenter);
   text_layer_set_text_color(&month_layer, GColorWhite);
   text_layer_set_background_color(&month_layer, GColorClear);
@@ -141,7 +129,6 @@ void handle_deinit(AppContextRef ctx) {
   (void)ctx;
 
   bmp_deinit_container(&background_image_container);
-  rotbmp_pair_deinit_container(&minute_hand_image_container);
   rotbmp_pair_deinit_container(&hour_hand_image_container);
 	
   fonts_unload_custom_font(day_font);
@@ -154,11 +141,11 @@ void handle_tick(AppContextRef ctx, PebbleTickEvent *t){
   (void)ctx;
 
    if(t->tick_time->tm_min==0&&t->tick_time->tm_hour==0)
-     {
+   {
      draw_day();
      draw_date();
      draw_month();
-     }        
+   }        
 }
 
 // The main event/run loop for our app
